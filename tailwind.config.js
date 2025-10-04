adf81090d7eeeadd0ce00feb75c92e083e490d58
Jose Fernández Tamame <jftamames@gmail.com>
2025-10-04 20:05:29 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // La ruta más simple y efectiva para Next.js App Router
     './app/**/*.{js,ts,jsx,tsx,mdx}', 
     './content/**/*.{md,mdx}',
-    // Esto cubre todos los archivos, incluyendo los componentes en app/(components)
   ],
   theme: { 
     extend: {} 
